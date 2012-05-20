65502c4d2635df4108604975239df2c3688dc7e0
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2012-05-20 20:31:38 +0000

    Gradient: impl

--- src/symbolic/ibex_Gradient.h
+++ src/symbolic/ibex_Gradient.h
@@ -120,33 +120,33 @@ private:
 	inline void symbol_bwd(const ExprSymbol&,                                       const GradLabel& result) { /* nothing to do */ }
 	inline void cst_bwd   (const ExprConstant&,                                     const GradLabel& result) { /* nothing to do */ }
 	inline void apply_bwd (const ExprApply&,   GradLabel** argL,                    const GradLabel& result) {  }
-	inline void add_bwd   (const ExprAdd&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void mul_bwd   (const ExprMul&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void sub_bwd   (const ExprSub&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void div_bwd   (const ExprDiv&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void max_bwd   (const ExprMax&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void min_bwd   (const ExprMin&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void atan2_bwd (const ExprAtan2& e, GradLabel& leftL, GradLabel& rightL, const GradLabel& result) {  }
-	inline void minus_bwd (const ExprMinus& e, GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void sign_bwd  (const ExprSign& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void abs_bwd   (const ExprAbs& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void power_bwd (const ExprPower& e, GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void sqr_bwd   (const ExprSqr& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void sqrt_bwd  (const ExprSqrt& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void exp_bwd   (const ExprExp& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void log_bwd   (const ExprLog& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void cos_bwd   (const ExprCos& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void sin_bwd   (const ExprSin& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void tan_bwd   (const ExprTan& e,   GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void cosh_bwd  (const ExprCosh& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void sinh_bwd  (const ExprSinh& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void tanh_bwd  (const ExprTanh& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void acos_bwd  (const ExprAcos& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void asin_bwd  (const ExprAsin& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void atan_bwd  (const ExprAtan& e,  GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void acosh_bwd (const ExprAcosh& e, GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void asinh_bwd (const ExprAsinh& e, GradLabel& exprL,                    const GradLabel& result) {  }
-	inline void atanh_bwd (const ExprAtanh& e, GradLabel& exprL,                    const GradLabel& result) {  }
+	inline void add_bwd   (const ExprAdd&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { leftL.g += result.g;              rightL.g += result.g; }
+	inline void mul_bwd   (const ExprMul&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { leftL.g += result.g * rightL.i(); rightL.g += result.g * leftL.i(); }
+	inline void sub_bwd   (const ExprSub&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { leftL.g += result.g;              rightL.g += -result.g;}
+	inline void div_bwd   (const ExprDiv&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { leftL.g += result.g / rightL.i(); rightL.g += result.g*(-leftL.i())/sqr(rightL.i()); }
+	inline void max_bwd   (const ExprMax&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { /* not implemented yet */ assert(false); }
+	inline void min_bwd   (const ExprMin&,     GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { /* not implemented yet */ assert(false); }
+	inline void atan2_bwd (const ExprAtan2& e, GradLabel& leftL, GradLabel& rightL, const GradLabel& result) { /* not implemented yet */ assert(false); }
+	inline void minus_bwd (const ExprMinus& e, GradLabel& exprL,                    const GradLabel& result) { exprL.g += -1.0*result.g; }
+	inline void sign_bwd  (const ExprSign& e,  GradLabel& exprL,                    const GradLabel& result) { /* not implemented yet */ assert(false); }
+	inline void abs_bwd   (const ExprAbs& e,   GradLabel& exprL,                    const GradLabel& result) { /* not implemented yet */ assert(false); }
+	inline void power_bwd (const ExprPower& e, GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * e.expon * pow(exprL.i(), e.expon-1); }
+	inline void sqr_bwd   (const ExprSqr& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 2.0 * exprL.i(); }
+	inline void sqrt_bwd  (const ExprSqrt& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 0.5 / sqrt(exprL.i());  }
+	inline void exp_bwd   (const ExprExp& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * exp(exprL.i()); }
+	inline void log_bwd   (const ExprLog& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g / exprL.i(); }
+	inline void cos_bwd   (const ExprCos& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * -sin(exprL.i()); }
+	inline void sin_bwd   (const ExprSin& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * cos(exprL.i()); }
+	inline void tan_bwd   (const ExprTan& e,   GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * (1.0 + sqr(tan(exprL.i()))); }
+	inline void cosh_bwd  (const ExprCosh& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * sinh(exprL.i()); }
+	inline void sinh_bwd  (const ExprSinh& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * cosh(exprL.i()); }
+	inline void tanh_bwd  (const ExprTanh& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * (1.0 - sqr(tanh(exprL.i()))); }
+	inline void acos_bwd  (const ExprAcos& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * -1.0 / sqrt(1.0-sqr(exprL.i())); }
+	inline void asin_bwd  (const ExprAsin& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 1.0 / sqrt(1.0-sqr(exprL.i())); }
+	inline void atan_bwd  (const ExprAtan& e,  GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 1.0 / (1.0+sqr(exprL.i())); }
+	inline void acosh_bwd (const ExprAcosh& e, GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 1.0 / sqrt(sqr(exprL.i()) -1.0); }
+	inline void asinh_bwd (const ExprAsinh& e, GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 1.0 / sqrt(1.0+sqr(exprL.i())); }
+	inline void atanh_bwd (const ExprAtanh& e, GradLabel& exprL,                    const GradLabel& result) { exprL.g += result.g * 1.0 / (1.0-sqr(exprL.i())); }
 
 };
 
