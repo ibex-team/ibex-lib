72c3eeea421f4c7f1c214caa1b951103390cdb5c
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2012-07-20 13:31:20 +0000

    CtcInteger: add

---
+++ src/contractor/ibex_CtcInteger.cpp
@@ -0,0 +1,47 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : ibex_CtcInteger.cpp
+// Author      : Gilles Chabert
+// Copyright   : Ecole des Mines de Nantes (France)
+// License     : See the LICENSE file
+// Created     : Jul 20, 2012
+// Last Update : Jul 20, 2012
+//============================================================================
+
+#include "ibex_CtcInteger.h"
+
+namespace ibex {
+
+CtcInteger::CtcInteger(int nb_var, const BoolMask& is_int) : Ctc(nb_var), is_int(is_int) {
+	for (int v=0; v<nb_var; v++)
+		output[v]=input[v]=is_int[v];
+}
+
+void CtcInteger::contract(IntervalVector& box) {
+	for (int i=0; i<nb_var; i++) {
+		if (!is_int[i]) continue;
+		proj_integer(box[i]);
+		if (box[i].is_empty()) {
+			box.set_empty();
+			throw EmptyBoxException();
+		}
+	}
+}
+
+void CtcInteger::contract(IntervalVector& box, const BoolMask& impact) {
+	for (int i=0; i<nb_var; i++) {
+		if (is_int[i] && impact[i]) {
+			proj_integer(box[i]);
+			if (box[i].is_empty()) {
+				box.set_empty();
+				throw EmptyBoxException();
+			}
+		}
+	}
+}
+
+bool CtcInteger::indempotent() {
+	return true;
+}
+
+} // end namespace ibex
