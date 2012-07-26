92fe1f1a9f4d93a1c09ca0f2fbd8e58ac2e9caa7
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2012-07-26 14:40:12 +0000

    distrib: fix pb install Darwin

--- distrib/script/install.sh
+++ distrib/script/install.sh
@@ -3,10 +3,22 @@
 echo installing ibex...
 
 IBEXPATH=`pwd`
+KERNEL=`uname`
 
 TARGET_DIR=/usr/local/include
 TARGET_LINK=$TARGET_DIR/ibex
 SOURCE_LINK=$IBEXPATH/include
+
+if [ "$KERNEL" == "Linux" ]; then
+  echo OS identified as Linux;
+  DYLIB=libibex.so
+elif [ "$KERNEL" == "Darwin" ]; then
+  echo OS identified as Darwin;
+  DYLIB=libibex.dylib
+else 
+  echo Unknown architecture: $KERNEL
+  exit 1
+fi
 
 # ============================================== 
 ./script/unlink.sh $SOURCE_LINK $TARGET_DIR $TARGET_LINK 
@@ -21,8 +33,8 @@ echo done.
 # ============================================== 
 
 TARGET_DIR=/usr/local/lib
-TARGET_LINK=$TARGET_DIR/libibex.so
-SOURCE_LINK=$IBEXPATH/lib/libibex.so
+TARGET_LINK=$TARGET_DIR/$DYLIB
+SOURCE_LINK=$IBEXPATH/lib/$DYLIB
 
 # ============================================== 
 ./script/unlink.sh $SOURCE_LINK $TARGET_DIR $TARGET_LINK 
@@ -36,8 +48,10 @@ ln -s $SOURCE_LINK $TARGET_LINK
 echo done.
 # ============================================== 
 
-echo -n \\t Running ldconfig ..........
-ldconfig
-echo done.
+if [ "$KERNEL" == "Linux" ]; then
+  echo -n \\t Running ldconfig ..........
+  ldconfig
+  echo done.
+endif
 
 echo done.
--- distrib/script/uninstall.sh
+++ distrib/script/uninstall.sh
@@ -8,6 +8,17 @@ TARGET_DIR=/usr/local/include
 TARGET_LINK=$TARGET_DIR/ibex
 SOURCE_LINK=$IBEXPATH/include
 
+if [ "$KERNEL" == "Linux" ]; then
+  echo OS identified as Linux;
+  DYLIB=libibex.so
+elif [ "$KERNEL" == "Darwin" ]; then
+  echo OS identified as Darwin;
+  DYLIB=libibex.dylib
+else 
+  echo Unknown architecture: $KERNEL
+  exit 1
+fi
+
 # ============================================== 
 ./script/unlink.sh $SOURCE_LINK $TARGET_DIR $TARGET_LINK 
 
@@ -17,8 +28,8 @@ fi
 # ============================================== 
 
 TARGET_DIR=/usr/local/lib
-TARGET_LINK=$TARGET_DIR/libibex.so
-SOURCE_LINK=$IBEXPATH/lib/libibex.so
+TARGET_LINK=$TARGET_DIR/$DYLIB
+SOURCE_LINK=$IBEXPATH/lib/$DYLIB
 
 # ============================================== 
 ./script/unlink.sh $SOURCE_LINK $TARGET_DIR $TARGET_LINK 
@@ -28,8 +39,10 @@ if [ $? != 0 ]; then
 fi
 # ============================================== 
 
-echo -n \\t Running ldconfig ..........
-ldconfig
-echo done.
+if [ "$KERNEL" == "Linux" ]; then
+  echo -n \\t Running ldconfig ..........
+  ldconfig
+  echo done.
+endif
 
 echo done.
