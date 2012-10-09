cfdc282cea5176090af9251c2beb6c42521403c2
Anthony Baire <anthony.baire@irisa.fr>
2012-10-09 15:37:53 +0000

    support building with the flex provided in mingw/msys

--- flex.py
+++ flex.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
-import waflib.TaskGen, re
+import waflib.TaskGen, os, re, sys
 def decide_ext(self,node):
 	if'cxx'in self.features:
 		return['.lex.cc']
@@ -16,7 +16,10 @@ def flexfun(tsk):
 	tsk.last_cmd=lst=[]
 	lst.extend(to_list(env['FLEX']))
 	lst.extend(to_list(env['FLEXFLAGS']))
-	lst.extend([a.path_from(bld.bldnode)for a in tsk.inputs])
+	inputs = [a.path_from(bld.bldnode)for a in tsk.inputs]
+	if env.FLEX_MSYS:
+		inputs = [x.replace(os.sep, '/') for x in inputs]
+	lst.extend(inputs)
 	lst=[x for x in lst if x]
 	txt=bld.cmd_and_log(lst,cwd=wd,env=env.env or None,quiet=0)
 	tsk.outputs[0].write(re.sub('\r\n?|\n\r', '\n', txt))
@@ -24,3 +27,6 @@ waflib.TaskGen.declare_chain(name='flex',rule=flexfun,ext_in='.l',decider=decide
 def configure(conf):
 	conf.find_program('flex',var='FLEX')
 	conf.env.FLEXFLAGS=['-t']
+
+	if sys.platform == "win32" and re.search (r"\\msys\\[0-9.]+\\bin\\flex.exe$", conf.env.FLEX):
+		conf.env.FLEX_MSYS = True
