39f0e511c61b03245fa7afd3785b52ebc4c0ed06
Bertrand Neveu <bertrand.neveu@enpc.fr>
2012-10-12 14:02:05 +0000

    max_diam_box added in IbexAffineSimplex.h

--- src/IbexAffineSimplex.h
+++ src/IbexAffineSimplex.h
@@ -28,8 +28,8 @@ class AA_Simplex : public LR_contractor {
  */
  
     AA_Simplex(const System& sys, Contractor* ctc, int goal_ctr=-1, REAL ratio_fp=default_ratio_fp, REAL ratio_fp2=default_ratio_fp2, 
-    REAL var_min_width=default_var_min_width, ctc_mode cmode=ALL_BOX, int max_iter_soplex=100) : 
-    LR_contractor(sys, ctc, goal_ctr,  ratio_fp, ratio_fp2, var_min_width, cmode, max_iter_soplex), Operator(sys.space)   {
+    REAL var_min_width=default_var_min_width, ctc_mode cmode=ALL_BOX, int max_iter_soplex=100, REAL max_diam_box= default_max_diam_box) : 
+    LR_contractor(sys, ctc, goal_ctr,  ratio_fp, ratio_fp2, var_min_width, cmode, max_iter_soplex, max_diam_box), Operator(sys.space)   {
 
      var=new AF2_expr[sys.space.nb_var()];
      evls= new EvaluatorAffine*[sys.nb_ctr()];
