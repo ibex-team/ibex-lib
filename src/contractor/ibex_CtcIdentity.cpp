0bbc64d5dcc571b92080f853601f6cee20f52dd7
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2013-02-19 14:44:44 +0000

    Identity: add

---
+++ src/contractor/ibex_CtcIdentity.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : ibex_CtcIdentity.cpp
+// Author      : Gilles Chabert
+// Copyright   : Ecole des Mines de Nantes (France)
+// License     : See the LICENSE file
+// Created     : Feb 18, 2013
+// Last Update : Jul 18, 2013
+//============================================================================
+
+#include "ibex_CtcIdentity.h"
+
+namespace ibex {
+
+CtcIdentity::CtcIdentity(int n) : Ctc(n) { }
+
+void CtcIdentity::contract(IntervalVector& box) { }
+
+} // end namespace ibex
---
+++ src/contractor/ibex_CtcIdentity.h
@@ -0,0 +1,34 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : ibex_CtcIdentity.h
+// Author      : Gilles Chabert
+// Copyright   : Ecole des Mines de Nantes (France)
+// License     : See the LICENSE file
+// Created     : May 14, 2012
+// Last Update : May 14, 2012
+//============================================================================
+
+#ifndef __IBEX_CTC_IDENTITY_H__
+#define __IBEX_CTC_IDENTITY_H__
+
+#include "ibex_Ctc.h"
+
+namespace ibex {
+
+/** \ingroup contractor
+ * \brief Identity contractor
+ *
+ */
+class CtcIdentity : public Ctc {
+public:
+
+	CtcIdentity(int n);
+
+	/**
+	 * \brief Contract a box.
+	 */
+	virtual void contract(IntervalVector& box);
+};
+
+} // end namespace ibex
+#endif // __IBEX_CTC_IDENTITY_H__
