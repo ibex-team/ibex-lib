5676220992a59aa5407925c672e112e5fb662279
Benoit DESROCHERS <ben.ensta@gmail.com>
2015-09-26 10:01:18 +0200

    issue #150 Change BoolInterval, use Luc Jaulin implementation

--- src/predicate/ibex_BoolInterval.h
+++ src/predicate/ibex_BoolInterval.h
@@ -1,11 +1,11 @@
 //============================================================================
 //                                  I B E X                                   
 // File        : ibex_BoolInterval.h
-// Author      : Gilles Chabert
+// Author      : Gilles Chabert, Luc Jaulin
 // Copyright   : Ecole des Mines de Nantes (France)
 // License     : See the LICENSE file
 // Created     : Nov 27, 2012
-// Last Update : Nov 27, 2012
+// Last Update : Sep 29, 2015
 //============================================================================
 
 #ifndef __IBEX_BOOL_INTERVAL_H__
@@ -22,35 +22,164 @@ namespace ibex {
  *
  * \brief Boolean interval.
  */
-typedef enum { NO, MAYBE, YES } BoolInterval;
+typedef enum { NO, MAYBE, YES, EMPTY} IBOOLEAN;
+
+class BoolInterval {
+public:
+	IBOOLEAN value;
+	BoolInterval ();
+	BoolInterval (bool);
+	BoolInterval (IBOOLEAN );
+	BoolInterval (const BoolInterval&);
+
+	bool		 			operator== (const BoolInterval& ) const;
+	bool		 			operator!= (const BoolInterval& ) const;	
+	
+	// And
+	BoolInterval  operator&&  (const BoolInterval&) const;
+	// Or
+	BoolInterval  operator||  (const BoolInterval&) const;
+	// Intersection
+	BoolInterval  operator&  (const BoolInterval&) const;
+	BoolInterval& operator&= (const BoolInterval&);
+	// Union
+	BoolInterval  operator|  (const BoolInterval&) const;
+	BoolInterval& operator|= (const BoolInterval&);
+	// Neg
+	BoolInterval  operator!  () const; 
+	
+};
+
+BoolInterval Inter(const BoolInterval&,const BoolInterval&);
+BoolInterval Not(const BoolInterval& x);
+BoolInterval Union(const BoolInterval&, const BoolInterval&);
+BoolInterval And(const BoolInterval&,const BoolInterval&);
+BoolInterval Or(const BoolInterval&,const BoolInterval&);
+BoolInterval leq(const BoolInterval& x, const BoolInterval& y);
+BoolInterval geq(const BoolInterval& x, const BoolInterval& y);
+BoolInterval Restrict(const BoolInterval& x, const BoolInterval& y);      // return x and !y
+BoolInterval Xor(const BoolInterval& x, const BoolInterval& y);
+
+//======================================================================
+//============== INLINE IMPLEMENTATION =================================
+//======================================================================
 
 
-/**
- * \brief x & y
- */
-BoolInterval operator&(const BoolInterval& x, const BoolInterval& y);
+inline BoolInterval::BoolInterval ()
+{value = MAYBE;}
 
+inline BoolInterval::BoolInterval (bool b)
+{if (b) value=YES; else value=NO;}
 
-/**
- * \brief x | y
- */
-BoolInterval operator|(const BoolInterval& x, const BoolInterval& y);
+inline BoolInterval::BoolInterval (IBOOLEAN tt) {value = tt;}
 
-/**
- * \brief x &= y
- */
-BoolInterval operator&=(BoolInterval& x, const BoolInterval& y);
+inline BoolInterval::BoolInterval (const BoolInterval& t)
+{*this = t;}
 
+inline BoolInterval BoolInterval::operator&& (const BoolInterval& y) const          
+{    return And(*this,y);  }
 
-/**
- * \brief x |= y
- */
-BoolInterval operator|=(BoolInterval& x, const BoolInterval& y);
+inline BoolInterval BoolInterval::operator|| (const BoolInterval& y) const          
+{    return Or(*this,y); }
 
-/**
- * \brief !x
- */
-BoolInterval operator!(const BoolInterval& x);
+inline BoolInterval BoolInterval::operator& (const BoolInterval& y) const                        
+{    return Inter(*this,y);  }
+
+inline BoolInterval BoolInterval::operator| (const BoolInterval& y) const        
+{    return Union(*this,y); }
+
+inline BoolInterval& BoolInterval::operator&= (const BoolInterval& y)
+{    this->value = (*this&y).value; return  *this; }
+
+inline BoolInterval& BoolInterval::operator|= (const BoolInterval& y)
+{    this->value = (*this|y).value; return  *this; }
+
+inline bool BoolInterval::operator== (const BoolInterval& y) const
+{	return (this->value==y.value);}
+
+inline bool BoolInterval::operator!= (const BoolInterval& y) const
+{	return (this->value!=y.value);}
+
+inline BoolInterval BoolInterval::operator! () const {
+	if (this->value == YES) return BoolInterval(NO);
+	if (this->value == NO) return BoolInterval(YES);
+	return this;
+}
+
+inline BoolInterval Not(const BoolInterval& x){
+	if (x == MAYBE)   return MAYBE;
+	if (x == YES)      return NO;
+	if (x == NO)     return YES;
+	return EMPTY;
+}
+
+inline BoolInterval And(const BoolInterval& x, const BoolInterval& y){
+	if ((x==EMPTY)||(y==EMPTY))   return BoolInterval(EMPTY);
+	if ((x==NO)||(y==NO)) return BoolInterval(NO);
+	if ((x==MAYBE)||(y==MAYBE)) return BoolInterval(MAYBE);
+	return BoolInterval(YES);
+}
+
+inline BoolInterval Or(const BoolInterval& x, const BoolInterval& y){
+	if ((x==EMPTY)||(y==EMPTY))   return BoolInterval(EMPTY);
+	if ( (x==YES) || (y==YES) ) return BoolInterval(YES);
+	if ( (x==MAYBE) || (y==MAYBE) ) return BoolInterval(MAYBE);
+	return BoolInterval(NO);
+}
+
+inline BoolInterval Inter(const BoolInterval& x,const BoolInterval& y){
+	if (x == y)   return x;
+	if (x == MAYBE)   return y;
+	if (y == MAYBE)   return x;
+	return BoolInterval(EMPTY);
+}
+
+inline BoolInterval Union(const BoolInterval& x,const BoolInterval& y){
+	if (x == EMPTY)   return y;
+ 	if (y == EMPTY)   return x;
+	if (x == y)   return x;
+	return BoolInterval(MAYBE);
+}
+
+inline BoolInterval Xor(const BoolInterval& x,const BoolInterval& y){
+	if (x == EMPTY)   return EMPTY;
+	if (y == EMPTY)   return EMPTY;
+	if (x == MAYBE)   return MAYBE;
+	if (y == MAYBE)   return MAYBE;
+	if (x == y)   return NO;
+	return BoolInterval(YES);
+}
+
+inline BoolInterval geq(const BoolInterval& x, const BoolInterval& y){     // Project the constraint x>=y with respect to x
+	BoolInterval r;
+        r=BoolInterval(MAYBE);
+        if (y==EMPTY)  r=BoolInterval(EMPTY);
+	if (y==YES) r=BoolInterval(YES);
+        return Inter(x,r);
+}
+
+inline BoolInterval leq(const BoolInterval& x, const BoolInterval& y){      // Project the constraint x<=y with respect to x
+	BoolInterval r;
+        r=BoolInterval(MAYBE);
+        if (y==EMPTY)  r=BoolInterval(EMPTY);
+	if (y==NO) r=BoolInterval(NO);
+        return Inter(x,r);
+/*                               a     &&    (implique b)
+1*0=\EMPTY                       1     &&      0
+1*1=1                            1     &&     [0,1]
+1*[0,1]=1                        1     &&     [0,1]
+0*0=0                            0     &&      0
+0*1=0                            0     &&      [0,1]
+0*[0,1]=0                        0     &&      [0,1]
+[0,1]*0=0                        [0,1] &&       0
+[0,1]*1=[0,1]                    [0,1] &&      [0,1]
+[0,1]*[0,1]=[0,1]                [0,1] &&     [0,1]
+*/
+}
+
+inline BoolInterval Restrict(const BoolInterval& x, const BoolInterval& y)      // return x and !y
+{ return And(x,!y);}
+
 
 } // namespace ibex
 
