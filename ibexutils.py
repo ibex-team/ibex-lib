a70f52dd22d1b253a0087e804a70e1c0df821191
gchabert <gilles.chabert@mines-nantes.fr>
2016-03-17 16:59:15 +0100

    Waf scripts refactoring: AMPL plugin

--- ibexutils.py
+++ ibexutils.py
@@ -2,11 +2,28 @@ import os, tarfile
 
 from waflib import Logs
 
-def unpack_archive (src_dir_node, name, filename = None):
-	src_dir = src_dir_node.abspath()
-	path = os.path.join (src_dir, name)
+# Parameters:
+# - src_dir_node  : source directory: where the archive is located
+# - name          : name of the directory where the file will be extracted
+# - filename      : filename of the archive (in src_dir)
+# - dest_dir_node : destination directory. If None, the file is extracted in src_dir
+# 
+# Return         : the node of the directory where the file has been extracted
+#
+def unpack_archive (src_dir_node, name, filename = None, dest_dir_node = None):
+	
+	if not dest_dir_node:
+		dest_dir_node = src_dir_node
+		
 
-	if not os.path.isdir (path):
+	src_dir = src_dir_node.abspath()
+
+	dest_dir = dest_dir_node.abspath()
+
+	# path is the destination folder where the file is extracted 
+	path = os.path.join (dest_dir, name)
+	
+	if not os.path.isdir (path): # if not already extracted
 		# extract the sources
 		os.makedirs (path)
 
@@ -15,9 +32,11 @@ def unpack_archive (src_dir_node, name, filename = None):
 
 		Logs.pprint ("NORMAL", "Unpacking %s" % filename)
 		t = tarfile.open (os.path.join (src_dir, filename))
-		t.extractall (src_dir)
-
-	node = src_dir_node.find_dir (name)
+		
+		t.extractall (dest_dir)
+	
+	node = dest_dir_node.find_dir (name)
+	
 	assert node
 	return node
 
