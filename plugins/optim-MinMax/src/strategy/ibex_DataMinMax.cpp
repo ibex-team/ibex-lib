77dbe7279379b50c4e338fc6db84feab3cd573ab
jordan08 <jordan.ninin@gmail.com>
2016-09-24 11:04:50 +0200

    add Backtracable structure for Minmax

---
+++ plugins/optim-MinMax/src/strategy/ibex_DataMinMax.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : ibex_DataMinMax.cpp
+// Author      : Jordan Ninin
+// License     : See the LICENSE file
+// Created     : Oct 18, 2014
+//============================================================================
+
+#include "ibex_DataMinMax.h"
+
+namespace ibex {
+
+DataMinMax::DataMinMax() : fmax(), pu(0), y_heap(y_heap_costf1,false,y_heap_costf2,false) {
+
+}
+
+DataMinMax::DataMinMax(const DataMinMax& e) : fmax(e.fmax), pu(e.pu), y_heap(e.y_heap) {
+
+}
+
+std::pair<Backtrackable*,Backtrackable*> DataMinMax::down() {
+	return std::pair<Backtrackable*,Backtrackable*>(new DataMinMax(*this),new DataMinMax(*this));
+}
+
+DataMinMax::~DataMinMax() {
+
+}
+
+
+double x_heap_costflb::cost(const Cell& elem) const {
+    return elem.get<DataMinMax>().fmax.lb();
+}
+
+
+void x_heap_costflb::add_backtrackable(Cell& root) {
+	root.add<DataMinMax>();
+}
+
+} // end namespace ibex
---
+++ plugins/optim-MinMax/src/strategy/ibex_DataMinMax.h
@@ -0,0 +1,96 @@
+//============================================================================
+//                                  I B E X                                   
+// File        : ibex_DataMinMax.h
+// Author      : Jordan Ninin, Gilles Chabert
+// License     : See the LICENSE file
+// Created     : Oct 18, 2014
+//============================================================================
+
+#ifndef __IBEX_OPTIM_DATA_H__
+#define __IBEX_OPTIM_DATA_H__
+
+#include "ibex_Backtrackable.h"
+#include "ibex_Interval.h"
+#include "ibex_DoubleHeap.h"
+#include "ibex_CellCostFunc.h"
+
+namespace ibex {
+
+/**
+ * \ingroup strategy
+ *
+ * \brief Data required for the Optimizer
+ */
+class DataMinMax : public Backtrackable {
+
+protected:
+
+	DataMinMax(const DataMinMax& e);
+
+	/**
+	 * Cost function of the heap to store the element of the light solver
+	 */
+	static CellCostPFub y_heap_costf1;
+	static CellCostPFlb y_heap_costf2;
+
+public:
+	/**
+	 * \brief Constructor for the root node (followed by a call to init_root).
+	 */
+	DataMinMax();
+
+	/**
+	 * \brief Delete *this.
+	 */
+	~DataMinMax();
+
+	/**
+	 * \brief Duplicate the structure into the left/right nodes
+	 */
+	std::pair<Backtrackable*,Backtrackable*> down();
+
+
+    /**
+     * Enclosure of maximum of the objective function
+     */
+    Interval fmax;
+
+	/**
+	 * \brief Casado criterion
+	 *
+	 * Constraint factor of the current box : between 0 infeasible and 1 for all constraints satisfied.
+	 */
+	double pu;
+
+	/**
+	 * y_heap inherited from father of box
+	 */
+    DoubleHeap<Cell> y_heap;
+
+
+};
+
+
+class x_heap_costflb : public CostFunc<Cell> { // element are sorted from the lowest lb of the evaluation of the objective function to the greatest
+public:
+
+	/**
+	 * \brief Return the cost associated to "data"
+	 */
+	virtual double cost(const Cell& elem) const;
+
+	/**
+	 * \brief Add backtrackable data required by this cost function
+	 *
+	 * This function is called for the root cell (before a
+	 * strategy is executed).
+	 *
+	 * Does nothing by default.
+	 */
+     virtual void add_backtrackable(Cell& root);
+
+};
+
+
+} // end namespace ibex
+#endif // __IBEX_OPTIM_DATA_H__
