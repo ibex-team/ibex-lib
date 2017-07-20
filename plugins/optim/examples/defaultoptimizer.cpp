727d4c3bd54fe44af9e9b4a08f5c8ebd6051f4c9
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2017-07-20 17:59:45 +0200

    [optim refact] New defaultoptimizer (started)

--- plugins/optim/examples/defaultoptimizer.cpp
+++ plugins/optim/examples/defaultoptimizer.cpp
@@ -1,54 +1,91 @@
+//============================================================================
+//                                  I B E X
+//               Default optimizer
+//
+// Author      : Gilles Chabert
+// Copyright   : IMT Atlantique (France)
+// License     : See the LICENSE file
+// Created     : Jul 09, 2017
+//============================================================================
+
 #include "ibex.h"
+
 #include <sstream>
+#include "args.hxx"
 
 #ifndef _IBEX_WITH_OPTIM_
-#error "You need the plugin Optim to run this example."
+#error "You need the IbexOpt plugin to run this program."
 #endif
 
 using namespace std;
 using namespace ibex;
 
-double convert(const char* argname, const char* arg) {
-	char* endptr;
-	double val = strtod(arg,&endptr);
-	if (endptr!=arg+strlen(arg)*sizeof(char)) {
-		stringstream s;
-		s << "\"" << argname << "\" must be a real number";
-		ibex_error(s.str().c_str());
-	}
-	return val;
-}
+const double default_timeout=60;
 
 int main(int argc, char** argv) {
+	args::ArgumentParser parser("********* IbexOpt (defaultoptimizer) *********.", "Solve a Minibex file.");
+	args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+	args::ValueFlag<double> timeout(parser, "float", "Timeout (time in seconds). Default value is 60s.", {'t', "timeout"});
+	args::ValueFlag<double> rel_epsf(parser, "float", "Relative precision on the objective. Default value is 1e-3.", {'r', "rel-epsf"});
+	args::ValueFlag<double> abs_epsf(parser, "float", "Absolute precision on the objective. Default value is 1e-3.", {'a', "abs-epsf"});
+	args::ValueFlag<double> epsh(parser, "float", "Equality relaxation value. Default value is 1e-8", {"epsh"});
+	args::ValueFlag<double> rand_seed(parser, "float", "Random seed. Default value is 1.", {"random-seed"});
+	args::Flag rigor(parser, "rigor", "Activate rigor mode", {"rigor"});
+	args::Flag trace(parser, "trace", "Activate trace. Updates of loup/uplo are printed while minimizing.", {"trace"});
+	args::Flag verbose(parser, "verbose", "Report results in a more human-readable way.", {"verbose"});
+//	args::ValueFlagList<char> characters(parser, "characters", "The character flag", {'c'});
+
+	args::Positional<std::string> filename(parser, "filename", "The name of the MINIBEX file.");
+//	args::PositionalList<double> numbers(parser, "numbers", "The numbers position list");
+
+	try
+	{
+		parser.ParseCLI(argc, argv);
+	}
+	catch (args::Help&)
+	{
+		std::cout << parser;
+		return 0;
+	}
+	catch (args::ParseError& e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	}
+	catch (args::ValidationError& e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	}
+
+	if (filename.Get()=="") {
+		ibex_error("no input file (try defaultoptimizer --help)");
+		exit(1);
+	}
 
 	try {
 
-		// check the number of arguments
-		if (argc<5) {
-			ibex_error("usage: defaultoptimizer filename prec goal_prec timelimit");
-		}
-
 		// Load a system of equations
-		System sys(argv[1]);
+		System sys(filename.Get().c_str());
 
 		cout << "load file " << argv[1] << "." << endl;
-
-		double prec       = convert("prec",argv[2]);
-		double goal_prec  = convert("goal_prec",argv[3]);  // the required precision for the objective
-		double time_limit = convert("timelimit",argv[4]);
 
 		if (!sys.goal) {
 			ibex_error(" input file has not goal (it is not an optimization problem).");
 		}
 
 		// Build the default optimizer
-		DefaultOptimizer o(sys,prec,goal_prec);
+		DefaultOptimizer o(sys,0.0,rel_epsf.Get());
 
 		// This option limits the search time
-		o.timeout=time_limit;
+		if (timeout)
+			o.timeout=timeout.Get();
 
 		// This option prints each better feasible point when it is found
-		o.trace=0;
+		if (trace)
+			o.trace=trace.Get();
 
 		// display solutions with up to 12 decimals
 		cout.precision(12);
