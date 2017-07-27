971dbd398c1371b941e4183522f9e70a84115c25
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2017-07-27 11:36:11 +0200

    [optim refact] Changing y=f(x) to y>=f(x) in ExtendedSystem and adapt CtcHC4 to force equality

--- plugins/optim/src/strategy/ibex_DefaultOptimizer.cpp
+++ plugins/optim/src/strategy/ibex_DefaultOptimizer.cpp
@@ -35,21 +35,11 @@ namespace {
 const double default_relax_ratio = 0.2;
 
 int extended_system = -1;   // index in memory (-1=none)
-int normalized_system = -1; // index in memory (-1=none)
 
-// The two next functions are necessary because we need
-// the normalized and extended system to build
-// arguments of the base class constructor (ctc, bsc, loup finder, etc.)
+// The next function is necessary because we need
+// an extended system to build arguments of the base
+// class constructor (ctc, bsc, loup finder, etc.)
 // and we don't know which argument is evaluated first
-
-NormalizedSystem& get_norm_sys(const System& sys, double eps_h) {
-	if (normalized_system>0)
-		return (NormalizedSystem&) *((*memory())->sys[normalized_system]); // already built and recorded
-	else {
-		normalized_system = (*memory())->sys.size();
-		return rec(new NormalizedSystem(sys,eps_h));
-	}
-}
 
 ExtendedSystem& get_ext_sys(const System& sys, double eps_h) {
 	if (extended_system>0)
@@ -62,14 +52,16 @@ ExtendedSystem& get_ext_sys(const System& sys, double eps_h) {
 
 }
 
+#define EXT_SYS get_ext_sys(sys,eps_h)
+
 DefaultOptimizer::DefaultOptimizer(const System& sys, double eps_x, double rel_eps_f, double abs_eps_f, double eps_h, bool rigor, double random_seed) :
 		Optimizer(sys.nb_var,
-			  ctc(get_ext_sys(sys,eps_h)), // warning: we don't know which argument is evaluated first
-			  rec(new SmearSumRelative(get_ext_sys(sys,eps_h),eps_x)),
-			  rec(rigor? (LoupFinder*) new LoupFinderCertify(sys,rec(new LoupFinderDefault(get_norm_sys(sys,eps_h)))) :
-						 (LoupFinder*) new LoupFinderDefault(get_norm_sys(sys,eps_h))),
-			  (CellBufferOptim&) rec(new CellDoubleHeap(get_ext_sys(sys,eps_h))),
-			  get_ext_sys(sys,eps_h).goal_var(),
+			  ctc(EXT_SYS), // warning: we don't know which argument is evaluated first
+			  rec(new SmearSumRelative(EXT_SYS,eps_x)),
+			  rec(rigor? (LoupFinder*) new LoupFinderCertify(EXT_SYS,rec(new LoupFinderDefault(EXT_SYS))) :
+						 (LoupFinder*) new LoupFinderDefault(EXT_SYS)),
+			  (CellBufferOptim&) rec(new CellDoubleHeap(EXT_SYS)),
+			  EXT_SYS.goal_var(),
 			  eps_x,
 			  rel_eps_f,
 			  abs_eps_f) {
@@ -81,22 +73,21 @@ DefaultOptimizer::DefaultOptimizer(const System& sys, double eps_x, double rel_e
 	/* reset (for next DefaultOptimizer to be created) */
 	*memory() = NULL;
 	extended_system = -1;
-	normalized_system = -1;
 }
 
 Ctc&  DefaultOptimizer::ctc(const System& ext_sys) {
 	Array<Ctc> ctc_list(3);
 
 	// first contractor on ext_sys : incremental HC4 (propag ratio=0.01)
-	ctc_list.set_ref(0, rec(new CtcHC4 (ext_sys.ctrs,0.01,true)));
+	ctc_list.set_ref(0, rec(new CtcHC4 (ext_sys,0.01,true)));
 	// second contractor on ext_sys : "Acid" with incremental HC4 (propag ratio=0.1)
-	ctc_list.set_ref(1, rec(new CtcAcid (ext_sys,rec(new CtcHC4 (ext_sys.ctrs,0.1,true)),true)));
+	ctc_list.set_ref(1, rec(new CtcAcid (ext_sys,rec(new CtcHC4 (ext_sys,0.1,true)),true)));
 	// the last contractor is "XNewton"
 	if (ext_sys.nb_ctr > 1) {
 		ctc_list.set_ref(2,rec(new CtcFixPoint
 				(rec(new CtcCompo(
 						rec(new CtcPolytopeHull(rec(new LinearizerCombo (ext_sys,LinearizerCombo::XNEWTON)))),
-								rec(new CtcHC4(ext_sys.ctrs,0.01)))), default_relax_ratio)));
+								rec(new CtcHC4(ext_sys,0.01)))), default_relax_ratio)));
 	} else {
 		ctc_list.set_ref(2,rec(new CtcPolytopeHull(rec(new LinearizerCombo (ext_sys,LinearizerCombo::XNEWTON)))));
 	}
--- src/contractor/ibex_CtcHC4.cpp
+++ src/contractor/ibex_CtcHC4.cpp
@@ -10,10 +10,26 @@
 
 #include "ibex_CtcHC4.h"
 #include "ibex_CtcFwdBwd.h"
+#include "ibex_ExtendedSystem.h"
 
 namespace ibex {
 
 namespace {
+
+Array<Ctc> convert(const System& sys) {
+	const ExtendedSystem* ext_sys=dynamic_cast<const ExtendedSystem*>(&sys);
+
+	std::vector<Ctc*> vec;
+	for (int i=0; i<sys.ctrs.size(); i++) {
+		if (ext_sys && i==ext_sys->goal_ctr()) {
+			// we replace y>=f(x) by y=f(x).
+			vec.push_back(new CtcFwdBwd(sys.ctrs[i].f,EQ));
+		} else
+			vec.push_back(new CtcFwdBwd(sys.ctrs[i]));
+	}
+	return vec;
+}
+
 Array<Ctc> convert(const Array<NumConstraint>& csp) {
 	std::vector<Ctc*> vec;
 	for (int i=0; i<csp.size(); i++) {
@@ -28,7 +44,7 @@ CtcHC4::CtcHC4(const Array<NumConstraint>& csp, double ratio, bool incremental) 
 }
 
 CtcHC4::CtcHC4(const System& sys, double ratio, bool incremental) :
-				CtcPropag(convert(sys.ctrs), ratio, incremental) {
+				CtcPropag(convert(sys), ratio, incremental) {
 
 }
 
