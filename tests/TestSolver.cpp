b1933fe43f4f4eabf4a3216d7d3198e30c863d09
jordan08 <jordan.ninin@gmail.com>
2017-09-07 15:16:48 +0200

    test windows 4

--- tests/TestSolver.cpp
+++ tests/TestSolver.cpp
@@ -24,112 +24,117 @@ namespace ibex {
 void TestSolver::circle1() {
 	const ExprSymbol& x=ExprSymbol::new_("x");
 	const ExprSymbol& y=ExprSymbol::new_("y");
-cout << "1"<< endl;
+cout << "1.1"<< endl;
 	SystemFactory f;
 	f.add_var(x);
 	f.add_var(y);
-	cout << "2"<< endl;
+	cout << "1.2"<< endl;
 	f.add_ctr(sqr(x)+sqr(y)=1);
 	f.add_ctr(sqr(x-1)+sqr(y)=1);
-	cout << "3"<< endl;
+	cout << "1.3"<< endl;
 	double cospi6=0.5;
 	double sinpi6=::sqrt(3)/2;
-	cout << "4"<< endl;
+	cout << "1.4"<< endl;
 	double _sol1[]={cospi6,sinpi6};
 	double _sol2[]={cospi6,-sinpi6};
-	cout << "5"<< endl;
+	cout << "1.5"<< endl;
 
 	Vector sol1(2,_sol1);
 	Vector sol2(2,_sol2);
 	System sys(f);
-	cout << "6"<< endl;
+	cout << "1.6"<< endl;
 	RoundRobin rr(1e-3);
 	CellStack stack;
 	CtcHC4 hc4(sys);
 
-	cout << "7"<< endl;
+	cout << "1.7"<< endl;
 	Solver solver(sys,hc4,rr,stack);
 	solver.start(IntervalVector(2,Interval(-10,10)));
 	const Solver::Solution* sol;
 	bool res;
-	cout << "8"<< endl;
+	cout << "1.8"<< endl;
 
 	res=solver.next(sol);
 	CPPUNIT_ASSERT(res==true);
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol1));
 	CPPUNIT_ASSERT(sol->status==Solver::SOLUTION);
-	cout << "9"<< endl;
+	cout << "1.9"<< endl;
 
 	res=solver.next(sol);
 	CPPUNIT_ASSERT(res==true);
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol2));
 	CPPUNIT_ASSERT(sol->status==Solver::SOLUTION);
 
-	cout << "10"<< endl;
+	cout << "1.10"<< endl;
 	res=solver.next(sol);
-	cout << "11"<< endl;
+	cout << "1.11"<< endl;
 	CPPUNIT_ASSERT(res==false);
-	cout << "12"<< endl;
+	cout << "1.12"<< endl;
 }
 
 void TestSolver::circle2() {
-	cout << "1"<< endl;
+	cout << "2.1"<< endl;
 	const ExprSymbol& x=ExprSymbol::new_("x");
 	const ExprSymbol& y=ExprSymbol::new_("y");
-	cout << "2"<< endl;
+	cout << "2.2"<< endl;
 
 	SystemFactory f;
 	f.add_var(x);
 	f.add_var(y);
 	f.add_ctr(sqr(x)+sqr(y)=1);
 	f.add_ctr(sqr(x-2)+sqr(y)=1);
-	cout << "3"<< endl;
+	cout << "2.3"<< endl;
 	double _sol1[]={1,0};
 	Vector sol1(2,_sol1);
 	System sys(f);
 	RoundRobin rr(1e-3);
 	CellStack stack;
 	CtcHC4 hc4(sys);
-	cout << "4"<< endl;
+	cout << "2.4"<< endl;
 	Solver solver(sys,hc4,rr,stack);
-	cout << "5"<< endl;
+	cout << "2.5"<< endl;
 
 	solver.start(IntervalVector(2,Interval(-10,10)));
 	const Solver::Solution* sol;
 	bool res;
-	cout << "6"<< endl;
+	cout << "2.6"<< endl;
 
 	res=solver.next(sol);
-	cout << "7"<< endl;
+	cout << "2.7"<< endl;
 	CPPUNIT_ASSERT(res==true);
-	cout << "8"<< endl;
+	cout << "2.8"<< endl;
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol1));
-	cout << "9"<< endl;
+	cout << "2.9"<< endl;
 	CPPUNIT_ASSERT(sol->status==Solver::UNKNOWN);
-	cout << "10"<< endl;
+	cout << "2.10"<< endl;
 
 	res=solver.next(sol);
-	cout << "11"<< endl;
+	cout << "2.11"<< endl;
 	CPPUNIT_ASSERT(res==false);
-	cout << "12"<< endl;
+	cout << "2.12"<< endl;
 }
 
 void TestSolver::circle3() {
+	cout << "3.1"<< endl;
 	const ExprSymbol& x=ExprSymbol::new_("x");
 	const ExprSymbol& y=ExprSymbol::new_("y");
+	cout << "3.2"<< endl;
 
 	SystemFactory f;
 	f.add_var(x);
 	f.add_var(y);
 	f.add_ctr(sqr(x)+sqr(y)=1);
 	f.add_ctr(sqr(x-1)+sqr(y)=1);
+	cout << "3.3"<< endl;
 
 	double cospi6=0.5;
 	double sinpi6=(sqrt(Interval(3))/2).lb();
 	f.add_ctr(4*y*abs(y)<=3); // a rigorous way to impose y<=sin(pi/6).
 
+	cout << "3.4"<< endl;
 	double _sol1[]={cospi6,sinpi6};
 	double _sol2[]={cospi6,-sinpi6};
+	cout << "3.5"<< endl;
 
 	Vector sol1(2,_sol1);
 	Vector sol2(2,_sol2);
@@ -138,29 +143,39 @@ void TestSolver::circle3() {
 	CellStack stack;
 	CtcHC4 hc4(sys);
 	Solver solver(sys,hc4,rr,stack);
+	cout << "3.6"<< endl;
 
 	solver.start(IntervalVector(2,Interval(-10,10)));
 	const Solver::Solution* sol;
 	bool res;
+	cout << "3.7"<< endl;
 
 	res=solver.next(sol);
+	cout << "3.8"<< endl;
 	CPPUNIT_ASSERT(res==true);
+	cout << "3.9"<< endl;
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol1));
 	CPPUNIT_ASSERT(sol->status==Solver::UNKNOWN);
+	cout << "3.10"<< endl;
 
 	res=solver.next(sol);
+	cout << "3.11"<< endl;
 	CPPUNIT_ASSERT(res==true);
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol2));
 	CPPUNIT_ASSERT(sol->status==Solver::SOLUTION);
+	cout << "3.12"<< endl;
 
 	res=solver.next(sol);
 	CPPUNIT_ASSERT(res==false);
+	cout << "3.13"<< endl;
 }
 
 void TestSolver::circle4() {
+	cout << "4.1"<< endl;
 	const ExprSymbol& x=ExprSymbol::new_("x");
 	const ExprSymbol& y=ExprSymbol::new_("y");
 	const ExprSymbol& r2=ExprSymbol::new_("r2");
+	cout << "4.2"<< endl;
 
 	SystemFactory f;
 	f.add_var(x);
@@ -168,13 +183,16 @@ void TestSolver::circle4() {
 	f.add_var(r2);
 	f.add_ctr(sqr(x)+sqr(y)=r2);
 	f.add_ctr(sqr(x-1)+sqr(y)=r2);
+	cout << "4.3"<< endl;
 
 	double cospi6=0.5;
 	double sinpi6=::sqrt(3)/2;
+	cout << "4.4"<< endl;
 
 	double _sol1[]={cospi6,sinpi6,1};
 	double _sol2[]={cospi6,-sinpi6,1};
 
+	cout << "4.5"<< endl;
 	Vector sol1(3,_sol1);
 	Vector sol2(3,_sol2);
 
@@ -182,10 +200,12 @@ void TestSolver::circle4() {
 	RoundRobin rr(1e-3);
 	CellStack stack;
 	CtcHC4 hc4(sys);
+	cout << "4.6"<< endl;
 
 	BitSet params(BitSet::empty(3));
 	params.add(2);
 	Solver solver(sys,params,hc4,rr,stack);
+	cout << "4.7"<< endl;
 
 	IntervalVector box(3);
 	box[0]=Interval(-10,10);
@@ -194,19 +214,26 @@ void TestSolver::circle4() {
 	solver.start(box);
 	const Solver::Solution* sol;
 	bool res;
+	cout << "4.8"<< endl;
 
 	res=solver.next(sol);
+	cout << "4.9"<< endl;
 	CPPUNIT_ASSERT(res==true);
+	cout << "4.10"<< endl;
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol1));
 	CPPUNIT_ASSERT(sol->status==Solver::SOLUTION);
+	cout << "4.11"<< endl;
 
 	res=solver.next(sol);
 	CPPUNIT_ASSERT(res==true);
+	cout << "4.12"<< endl;
 	CPPUNIT_ASSERT(sol->existence().is_superset(sol2));
 	CPPUNIT_ASSERT(sol->status==Solver::SOLUTION);
 
+	cout << "4.13"<< endl;
 	res=solver.next(sol);
 	CPPUNIT_ASSERT(res==false);
+	cout << "4.14"<< endl;
 }
 
 
