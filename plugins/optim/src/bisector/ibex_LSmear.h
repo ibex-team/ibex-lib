8c5346e8807b9a082df87e8da4a992f7712836e5
Gilles Chabert <gilles.chabert@mines-nantes.fr>
2017-10-04 17:20:12 +0200

    [lsmear] cosmetic changes

--- plugins/optim/src/bisector/ibex_LSmear.h
+++ plugins/optim/src/bisector/ibex_LSmear.h
@@ -1,26 +1,28 @@
-/*
- * ibex_LSmear.h
- *
- *  Created on: 25 may. 2017
- *      Author: iaraya
- */
+//============================================================================
+//                                  I B E X
+// File        : ibex_LSmear.h
+// Author      : Ignacio Araya
+// License     : See the LICENSE file
+// Last update : May, 25 2017
+//============================================================================
 
-#ifndef IBEX_LSMEAR
-#define IBEX_LSMEAR
+#ifndef __IBEX_LSMEAR__
+#define __IBEX_LSMEAR__
 
 #include "ibex_SmearFunction.h"
 #include "ibex_LPSolver.h"
 
 namespace ibex {
 
-typedef enum{LSMEAR=0, LSMEAR_MG} lsmear_mode;
+typedef enum{ LSMEAR=0, LSMEAR_MG } lsmear_mode;
 
 
 /**
  * \ingroup bisector
  *
- * \brief bisector which first ponderates the constraints by using the dual solution of a linear programming relaxation
- * and then computes the impact by using the Smear function heuristic.
+ * \brief bisector which first ponderates the constraints by using the dual
+ * solution of a linear programming relaxation and then computes the impact
+ * by using the Smear function heuristic.
  *
  */
 class LSmear : public SmearSumRelative {
@@ -28,17 +30,18 @@ class LSmear : public SmearSumRelative {
 public :
 	/**
 	 * \brief Create the LSmear bisector. See the article:
-	 * Araya, I., Neveu, B. lsmear: a variable selection strategy for interval branch and bound solvers (2017)
+	 * Araya, I., Neveu, B. lsmear: a variable selection strategy for interval
+	 * branch and bound solvers (2017)
 	 *
 	 * \param lsmode	- variant (see the paper)
 	 *
-     * For the parameters, see #SmearFunction::SmearFunction(System&, double, double).
+	 * For the parameters, see #SmearFunction::SmearFunction(System&, double, double).
 	 */
 	LSmear (System& sys,  double prec, double ratio=Bsc::default_ratio(),
 			lsmear_mode lsmode=LSMEAR_MG);
 
-	 /*
-	 * Variant with a vector of precisions.
+	/*
+	 * \brief Variant with a vector of precisions.
 	 *
 	 * \see #LSmear(System&, double, double, lsmear_mode)
 	 */
@@ -48,8 +51,7 @@ public :
 	/**
 	 * \brief Returns the variable to bisect.
 	 */
-	virtual int var_to_bisect(IntervalMatrix & J,const IntervalVector& box) const;
-
+	virtual int var_to_bisect(IntervalMatrix& J,const IntervalVector& box) const;
 
 	/**
 	 * \brief Computes the dual solution of the linear program mid(J).x<=0
@@ -60,7 +62,7 @@ public :
 	 * \param x 	- the current box
 	 * \param dual 	- the dual solution that will be returned
 	 */
-	LinearSolver::Status_Sol getdual(IntervalMatrix & J,const IntervalVector& x, Vector& dual) const;
+	LinearSolver::Status_Sol getdual(IntervalMatrix& J,const IntervalVector& x, Vector& dual) const;
 
 	/**
 	 * \brief The linear solver
@@ -78,16 +80,16 @@ public :
 
 
 inline LSmear::LSmear(System& sys,  double prec, double ratio, lsmear_mode lsmode) : SmearSumRelative(sys,prec,ratio),
-				lsmode(lsmode){
+		lsmode(lsmode) {
 	mylinearsolver = new LinearSolver(sys.nb_var);
 
 }
 
 inline LSmear::LSmear(System& sys, const Vector& prec, double ratio,lsmear_mode lsmode) : SmearSumRelative(sys,prec,ratio),
-				lsmode(lsmode) {
+		lsmode(lsmode) {
 	mylinearsolver = new LinearSolver(sys.nb_var);
 }
 
 } /* namespace ibex */
 
-#endif /* IBEX_LSMEAR */
+#endif /* __IBEX_LSMEAR__ */
