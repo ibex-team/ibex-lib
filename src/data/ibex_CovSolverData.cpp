f5f11cb058b1c3100d89b626c583817e50e32ce3
CHABERT Gilles <gilles.chabert@irt-jules-verne.fr>
2020-05-10 16:16:11 +0200

    Format id/version was mixed up in https://github.com/ibex-team/ibex-lib/commit/4aaf5367b6a3a375c4db8d32abfc7c7ecdf7244c + update TestCov

--- src/data/ibex_CovSolverData.cpp
+++ src/data/ibex_CovSolverData.cpp
@@ -17,11 +17,11 @@ using namespace std;
 
 namespace ibex {
 
-const unsigned int CovSolverData::FORMAT_VERSION = 1;
+const unsigned int CovSolverData::FORMAT_VERSION = 2;
 
 const unsigned int CovSolverData::subformat_level = 5;
 
-const unsigned int CovSolverData::subformat_number = 1;
+const unsigned int CovSolverData::subformat_number = 0;
 
 CovSolverData::CovSolverData(size_t n, size_t m, size_t nb_ineq, BoundaryType boundary_type, const vector<string>& var_names) :
 		CovManifold(n, m, nb_ineq, boundary_type), data(new Data()), own_data(true) {
@@ -169,6 +169,7 @@ ifstream* CovSolverData::read(const char* filename, CovSolverData& cov, std::sta
 	size_t nb_pending;
 
 	if (format_id.empty() || format_id.top()!=subformat_number || format_version.top()!=FORMAT_VERSION) {
+		cout << "here\n";
 		cov.data->_solver_solver_status = (unsigned int) Solver::SUCCESS;
 		cov.data->_solver_time = -1;
 		cov.data->_solver_nb_cells = 0;
