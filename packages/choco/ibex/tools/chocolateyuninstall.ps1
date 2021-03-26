4fff356a06f772da0887c28d6e87ee3095dfd816
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-03-26 21:13:37 +0100

    First attempt to generate .deb and .nupkg packages using GitHub actions

---
+++ packages/choco/ibex/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# Source registry key values which are shared between install and uninstall
+. $PSScriptRoot\regKeys.ps1
+
+if (Test-Path $CMakeRegistryPath) {
+  if (Test-Path $CMakeSystemRepositoryPath) {
+      Remove-Item "$CMakeSystemRepositoryPath\$CMakePackageName"
+  }
+}
---
+++ packages/choco/ibex/tools/regKeys.ps1
@@ -0,0 +1,3 @@
+$CMakeRegistryPath = "HKCU:\SOFTWARE\Kitware\CMake"
+$CMakeSystemRepositoryPath = "HKLM:\SOFTWARE\Kitware\CMake\Packages"
+$CMakePackageName = "Ibex"
