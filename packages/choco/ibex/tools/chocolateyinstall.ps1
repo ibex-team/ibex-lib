c2d24f2897e52b8523549fb87459a1491ccfd033
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-05-15 16:06:07 +0200

    Updated .nupkg

--- packages/choco/ibex/tools/chocolateyinstall.ps1
+++ packages/choco/ibex/tools/chocolateyinstall.ps1
@@ -6,5 +6,13 @@
 New-Item "$CMakeSystemRepositoryPath\$CMakePackageName" -ItemType directory -Force
 New-ItemProperty -Name "CMakePackageDir" -PropertyType String -Value "$env:ChocolateyPackageFolder\share\$CMakePackageName\cmake" -Path "$CMakeSystemRepositoryPath\$CMakePackageName" -Force
 
+# Get 32 bit binaries if needed and delete unnecessary files...
+if (Test-Path "$env:ChocolateyPackageFolder\x86") {
+	if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) {
+		Copy-Item -Recurse -Force -Path "$env:ChocolateyPackageFolder\x86\*" -Destination "$env:ChocolateyPackageFolder"
+	}
+	Remove-Item -Recurse -Force "$env:ChocolateyPackageFolder\x86"
+}
+
 Install-BinFile -Name libprim.a -Path "$env:ChocolateyPackageFolder\lib\ibex\3rd"
 Install-BinFile -Name libibex.a -Path "$env:ChocolateyPackageFolder\lib"
