7f5a9b34cc5a1a2f07f7155b1094dca0a1bc548e
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-08-10 17:36:19 +0200

    Updated packages generation

--- packages/choco/ibex/tools/chocolateyinstall.ps1
+++ packages/choco/ibex/tools/chocolateyinstall.ps1
@@ -3,16 +3,93 @@
 # Source variables which are shared between install and uninstall.
 . $PSScriptRoot\sharedVars.ps1
 
-New-Item "$CMakeSystemRepositoryPath\$CMakePackageName" -ItemType directory -Force
-New-ItemProperty -Name "CMakePackageDir" -PropertyType String -Value "$env:ChocolateyPackageFolder\share\$CMakePackageName\cmake" -Path "$CMakeSystemRepositoryPath\$CMakePackageName" -Force
+$pp = Get-PackageParameters
 
-# Get 32 bit binaries if needed and delete unnecessary files...
-if (Test-Path "$env:ChocolateyPackageFolder\x86") {
-	if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) {
-		Copy-Item -Recurse -Force -Path "$env:ChocolateyPackageFolder\x86\*" -Destination "$env:ChocolateyPackageFolder"
+$root = "$env:ChocolateyPackageFolder\.."
+
+if (!$pp['url']) { 
+	$url = 'https://github.com/lebarsfa/ibex-lib/releases/download/ibex-2/ibex_x86_mingw8.zip'
+	$checksum = '7855B0393DF389855E126E712C51DAB8649F19BE0101579C753FBEDF108B0132'
+	$url64 = 'https://github.com/lebarsfa/ibex-lib/releases/download/ibex-2/ibex_x64_mingw8.zip'
+	$checksum64 = '89C6BB0DE09BD86524947827B3ED88CBBDCAB63141AEE5F7FF273CFB416C0D22'
+	$packageArgs = @{
+		packageName   = $env:ChocolateyPackageName
+		unzipLocation = "$root"
+		url           = $url
+		url64bit      = $url64
+		checksum      = $checksum
+		checksumType  = 'sha256'
+		checksum64    = $checksum64
+		checksumType64= 'sha256'
 	}
-	Remove-Item -Recurse -Force "$env:ChocolateyPackageFolder\x86"
+	Install-ChocolateyZipPackage @packageArgs
+
+	$runtime = "mingw"
 }
+else
+{
+	$url = $pp['url']
+	#$checksum = $pp['sha256']
+	$packageArgs = @{
+		packageName   = $env:ChocolateyPackageName
+		unzipLocation = "$root"
+		url           = $url
+		#checksum      = $checksum
+		#checksumType  = 'sha256'
+	}
+	Install-ChocolateyZipPackage @packageArgs
+
+	# Analyze url to guess what to add to Windows PATH...
+	if ($url -match "x86") {
+		$arch = "x86"
+	}
+	else {
+		$arch = "x64"
+	}
+	if ($url -match "vc8") {
+		$runtime = "vc8"
+	}
+	if ($url -match "vc9") {
+		$runtime = "vc9"
+	}
+	elseif ($url -match "vc10") {
+		$runtime = "vc10"
+	}
+	elseif ($url -match "vc11") {
+		$runtime = "vc11"
+	}
+	elseif ($url -match "vc12") {
+		$runtime = "vc12"
+	}
+	elseif ($url -match "vc14") {
+		$runtime = "vc14"
+	}
+	elseif ($url -match "vc15") {
+		$runtime = "vc15"
+	}
+	elseif ($url -match "vc16") {
+		$runtime = "vc16"
+	}
+	elseif ($url -match "vc17") {
+		$runtime = "vc17"
+	}
+	elseif ($url -match "vc18") {
+		$runtime = "vc18"
+	}
+	else {
+		$runtime = "mingw"
+	}
+}
+
+if (!$pp['NoRegistry']) {
+	New-Item "$CMakeSystemRepositoryPath\$CMakePackageName" -ItemType directory -Force
+	New-ItemProperty -Name "CMakePackageDir" -PropertyType String -Value "$env:ChocolateyPackageFolder\share\$CMakePackageName\cmake" -Path "$CMakeSystemRepositoryPath\$CMakePackageName" -Force
+}
+#$pathtoadd = "$env:ChocolateyPackageFolder\bin"
+#if (!($pp['NoPath']) -and !([environment]::GetEnvironmentVariable("Path","Machine") -match [regex]::escape($pathtoadd))) {
+#	$newpath = [environment]::GetEnvironmentVariable("Path","Machine") + ";$pathtoadd"
+#	[environment]::SetEnvironmentVariable("Path",$newpath,"Machine")
+#}
 
 #Install-BinFile -Name libprim.a -Path "$env:ChocolateyPackageFolder\lib\ibex\3rd"
 #Install-BinFile -Name libibex.a -Path "$env:ChocolateyPackageFolder\lib"
--- packages/choco/ibex/tools/sharedVars.ps1
+++ packages/choco/ibex/tools/sharedVars.ps1
@@ -1,3 +1,4 @@
+if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) { $arch = "x86" } else { $arch = "x64" }
 $CMakeRegistryPath = "HKCU:\SOFTWARE\Kitware\CMake"
 $CMakeSystemRepositoryPath = "HKLM:\SOFTWARE\Kitware\CMake\Packages"
 $CMakePackageName = "Ibex"
