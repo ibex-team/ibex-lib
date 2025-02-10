41c4447e12f1727e06f5c7bdba8700bbfee03c2b
Gilles Chabert <gilchab@gmail.com>
2025-02-10 18:53:33 +0100

    Fix warning problems (update for Visual studio)

--- interval_lib_wrapper/gaol/ibex_IntervalLibWrapper.h
+++ interval_lib_wrapper/gaol/ibex_IntervalLibWrapper.h
@@ -3,7 +3,7 @@
 
 #if defined(_WIN32)
 	#pragma warning( push )
-	#pragma warning( disable : 4800 )
+	#pragma warning( disable )
 #elif defined(__GNUC__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wint-in-bool-context"
